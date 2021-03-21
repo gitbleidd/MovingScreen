b173192fb6c1752ff5fd2c1957dfb753c1a708f9
gitbleidd <adonis7952@hotmail.com>
2021-03-21 17:20:12 +0500

    Добавлено подробное логирование.

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from flask import Flask
 from flask import jsonify
 from datetime import datetime
 from threading import Thread
+from threading import Lock
 import time
+
+position_lock = Lock()
 
 # ---- Global vars ----
 
@@ -41,12 +44,14 @@ def read_frame():
     while True:
         current_byte = ser.read(1)
 
+        print(current_byte.hex(), end=' ')
         # Если попался символ экранирования , то запоминаем.
         if current_byte == b'\x7D':
             is_escape_char = True
             continue
 
         if is_frame:
+
             # Если предыдущий символ был символом экранизации, то надо преобразовать текущий.
             # Приводим текущий байт к int, делаем xor 0x20 и приводим обратно к bytes.
             # Пропускаем текущую итерацию, чтобы не посчтитало за конец фрейма.
@@ -56,16 +61,23 @@ def read_frame():
                 frame += current_byte
                 continue
 
+            #if (current_byte == b'~' and len(frame) < 4):
+            #    continue
+
             frame += current_byte
+
             if current_byte == b'~':  # Если дочитали до конца фрейма, то возращаем массив байтов.
 
                 data = frame[1:len(frame) - 3]  # Данные из из фрейма.
                 frame_crc = (frame[-2:-4:-1]).hex().lower()  # CRC16 из фрейма.
                 check_crc = hdlc_crc(data).lower()  # Пересчитанная CRC16.
 
+                #print(frame, time.time())
+                print("---", time.time(), end="")
+                print()
                 # Если CRC не совпадают, то значит пришел ошибочный фрейм.
                 if frame_crc != check_crc:
-                    raise CRCError('CRC error. Frame CRC: {0}. Check CRC: {1}'.format(str(frame_crc), str(check_crc)))
+                    raise CRCError('CRC error. Frame CRC: {0}. Check CRC: {1}. Full Frame {2}'.format(str(frame_crc), check_crc, str(frame)))
                 return data[:len(data)-1].decode()
 
         # Если пришло начало фрейма '~', то пробуем считать фрейм дальше.
@@ -91,8 +103,10 @@ def index():
 
 @app.route("/main")
 def test():
+    position_lock.acquire()
     global position
     data = {'name': 'Display position', 'position': position}
+    position_lock.release()
     return jsonify(data)
 
 
@@ -100,51 +114,40 @@ def position_updater():
     global ser
     global position
 
-    print("HDLC Reader started!")
+    local_position = 0
     while True:
         try:
             if ser is None:
                 raise serial.SerialException
-            position = int(read_frame())
-
-            local_var = int(read_frame())
+            local_position = int(read_frame())
         except serial.SerialException:
             try:
-                ser = serial.Serial('COM10', 9600, timeout=5)
+                if ser is None:
+                    ser = serial.Serial('COM10', 9600, timeout=0)
+                    ser.flush()
+                    continue
+                ser.close()
+                ser = serial.Serial('COM10', 9600, timeout=0)
                 ser.flush()
-            except Exception:
-                pass
+            except Exception as e:
+                print(e)
+                continue
         except CRCError as e:
             print(e)
         except FrameLengthError as e:
             print(e)
         except Exception as e:
             print(e)
-        #time.sleep(0.1)
 
-
-def position_updater_test():
-    global position
-    is_reverse = False
-    step = 1  # Сдвиг экрана за одну итерацию.
-    path_time = 10  # Время изменения координат от 0 до 100.
-    sleep_time = path_time * step / 100
-
-    while True:
-        if is_reverse:
-            position -= 1
-            if position == 0: is_reverse = False
-        else:
-            position += 1
-            if position == 100: is_reverse = True
-        time.sleep(sleep_time)
+        position_lock.acquire()
+        position = local_position
+        position_lock.release()
+        time.sleep(0.001)
 
 
 if __name__ == "__main__":
+    # Поток, который обновляет глобаль
     positionThread = Thread(target=position_updater, args=[])
     positionThread.start()
 
-    # positionThread = Thread(target=position_updater_test, args=[])
-    # positionThread.start()
-
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=False, threaded=True)
